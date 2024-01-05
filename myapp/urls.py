6f7baeff67b8d39e645da718839b19b8b017c1fa
Sidharth s <89461211+thesiddark@users.noreply.github.com>
2024-01-05 23:30:02 +0530

    template and views

--- myapp/urls.py
+++ myapp/urls.py
@@ -14,5 +14,15 @@ urlpatterns = [
     path('app reviews/',views.admin_view_appreviews),
     path('view criminals/',views.admin_view_criminals),
     path('view police/', views.admin_view_police),
-    path('register users/',views.admin_view_registered_users)
+    path('register users/',views.admin_view_registered_users),
+    #users
+    path('add family member/',views.user_add_family_members),
+    path('chat/',views.user_chat),
+    path('review/',views.user_review),
+    path('view and edit profile/',views.user_view_and_edit_profile),
+    path('view criminals/',views.user_view_criminals),
+    path('family members/',views.user_view_family_members),
+    #police
+    path('add criminals/',views.police_add_criminals),
+    path('police view criminals/',views.police_view_criminals),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
 
 def login(request):
     return render(request,'LOGIN.html')
@@ -39,36 +38,36 @@ def admin_complaints(request):
    return render(request,'ADMIN/admin complaints.html')
 
 def admin_complaints_post(request):
-    fromdate=request.POST['textfield']
-    todate = request.POST['textfield2']
+    fromdate=request.POST['from']
+    todate = request.POST['to']
     return render(request, 'ADMIN/admin complaints.html')
 
 def admin_edit_police(request):
    return render(request,'ADMIN/admin edit police.html')
 
 def admin_edit_police_post(request):
-    stationname=request.POST['textfield']
+    stationname=request.POST['station']
     siname = request.POST['siname']
     place = request.POST['place']
     post = request.POST['post']
     pin = request.POST['pin']
-    emailid = request.POST['emailid']
+    emailid = request.POST['email id']
     return HttpResponse("ok")
 
 def admin_suspicious_activity(request):
    return render(request,'ADMIN/admin suspicious activity.html')
 
 def admin_suspicious_activity_post(request):
-    fromdate = request.POST['textfield']
-    todate = request.POST['textfield2']
+    fromdate = request.POST['from']
+    todate = request.POST['to']
     return render(request, 'ADMIN/admin suspicious activity.html')
 
 def admin_view_appreviews(request):
    return render(request,'ADMIN/admin view AppReviews.html')
 
 def admin_view_appreviews_post(request):
-    fromdate = request.POST['textfield']
-    todate = request.POST['textfield2']
+    fromdate = request.POST['from']
+    todate = request.POST['to']
     return render(request, 'ADMIN/admin view AppReviews.html')
 
 def admin_view_criminals(request):
@@ -82,13 +81,87 @@ def admin_view_police(request):
    return render(request,'ADMIN/admin view police.html')
 
 def admin_view_police_post(request):
-    search = request.POST['textfield']
+    search = request.POST['name']
     return render(request, 'ADMIN/admin view police.html')
 
 def admin_view_registered_users(request):
    return render(request,'ADMIN/admin view registered users.html')
 
 def admin_view_registered_users_post(request):
-    search = request.POST['textfield']
+    search = request.POST['name']
     return render(request, 'ADMIN/admin view registered users.html')
-    
+
+#users
+
+def user_add_family_members(request):
+   return render(request,'user/user add family members.html')
+
+def user_add_family_members_post(request):
+    name = request.POST['name']
+    photo = request.POST['photo']
+    relation = request.POST['relation']
+    place = request.POST['place']
+    phone = request.POST['phone']
+    emailid = request.POST['emailid']
+    return HttpResponse("ok")
+
+def user_chat(request):
+   return render(request,'user/user chat.html')
+
+def user_chat_post(request):
+    fromdate = request.POST['from']
+    todate = request.POST['to']
+    message = request.POST['message']
+    return HttpResponse("ok")
+
+def user_review(request):
+   return render(request,'user/user review.html')
+def user_review_post(request):
+    review = request.POST['review']
+    return HttpResponse("ok")
+
+def user_view_and_edit_profile(request):
+   return render(request,'user/user view and edit profile.html')
+
+def user_view_and_edit_profile_post(request):
+    name = request.POST['name']
+    emailid = request.POST['email id']
+    phone = request.POST['phone']
+    place = request.POST['PLACE']
+    post = request.POST['post']
+    pin = request.POST['pin']
+    district = request.POST['district']
+    gender = request.POST['gender']
+    return HttpResponse("ok")
+
+def user_view_criminals(request):
+   return render(request,'user/user view criminals.html')
+
+def user_view_criminals_post(request):
+    name = request.POST['name']
+    return render(request,'user/user view criminals.html')
+
+def user_view_family_members(request):
+   return render(request,'user/user view family member.html')
+
+def user_view_family_members_post(request):
+    name = request.POST['name']
+    return render(request,'user/user view family member.html')
+
+#police
+
+def police_add_criminals(request):
+   return render(request,'police/police add criminals.html')
+
+def police_add_criminals_post(request):
+    name = request.POST['name']
+    photo = request.POST['photo']
+    details = request.POST['details']
+    return HttpResponse("ok")
+
+def police_view_criminals(request):
+   return render(request,'police/police view criminals.html')
+
+def police_view_criminals_post(request):
+    name = request.POST['name']
+    return render(request,'police/police view criminals.html')
